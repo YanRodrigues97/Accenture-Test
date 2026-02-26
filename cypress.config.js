b32a6acf9df7f5b29827e9acfc56ed81761c4c54
YanRodrigues97 <yan.moura@aluno.afya.com.br>
2026-02-26 00:24:07 -0300

    feat: initial project setup with configuration files

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://demoqa.com',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    video: false,
+    screenshotOnRunFailure: false,
+  },
+});
+
