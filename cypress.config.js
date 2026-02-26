8b0f4773dd2d4a7b128af28587f765b758a68743
YanRodrigues97 <yan.moura@aluno.afya.com.br>
2026-02-26 14:44:18 -0300

    feat: add cypress e2e test

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,15 @@ module.exports = defineConfig({
     specPattern: 'cypress/e2e/**/*.cy.js',
     video: false,
     screenshotOnRunFailure: false,
+    chromeWebSecurity: false,
+    requestTimeout: 10000,
+    setupNodeEvents(on, config) {      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        }
+      });      return config;
+    }
   },
 });
 
